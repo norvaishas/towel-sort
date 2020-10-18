2d20870d85b94395affcc727f1a05036082c09e0
Sergei Norvaishas <snorvaishas@yandex.ru>
2020-10-18 12:26:58 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    const sortMatrix = [];
+
+    if (!matrix || matrix.length === 0) {
+        return [];
+    }
+
+    matrix.forEach((elem, index) => {
+        if (index % 2 !== 0) {
+            sortMatrix.push(elem.reverse());
+        } else {
+            sortMatrix.push(elem);
+        }
+    });
+
+    return sortMatrix.join().split(',');
 }
